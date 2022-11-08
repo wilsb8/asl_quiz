beadce3adfc28e25d7c36af780324b3cab5f2756
wilsb8 <wilsb8@gmail.com>
2022-11-08 11:37:12 -0800

    twig

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 const quizzesCtrl = require('./src/controllers/quizzes');
 const questCtrl = require('./src/controllers/questions');
 const choiceCtrl = require('./src/controllers/choices');
+const { Quiz } = require('./src/models');
 
+app.set('views', __dirname = './src/views')
+app.set('view engine', 'twig')
 
 // GET /products HTTP/1.1
-app.get('/', (req, res) =>  {
-    res.send('<h1>Home Page</h1>');
+app.get('/', async (req, res) => {
+    const quiz = await Quiz.findByPk(1)
+    res.render('home/home', {quiz});
 });
 
 app.use('/quizzes', quizzesCtrl);
