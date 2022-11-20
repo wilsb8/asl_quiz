bb4662bc65ef978707e331a1d3aa3a1a51bd2374
William A. Ferguson <wilsb8@gmail.com>
2022-11-20 14:01:16 +0000

    "React Project"

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const choiceCtrl = require('./src/controllers/choices');
 const authCtrl = require('./src/controllers/auth')
 const { Quiz } = require('./src/models');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const session = require('express-session');
 const { response } = require('express');
 
@@ -15,6 +16,8 @@ app.use(session({
     cookie: { maxAge: 60000 }
 }))
 // middelware
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', __dirname = './src/views')
 app.set('view engine', 'twig')
