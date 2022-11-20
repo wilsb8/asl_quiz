16d3870f0479b6d776b07c16bf2aa22afff18313
wilsb8 <wilsb8@gmail.com>
2022-11-20 09:51:00 -0800

    React Project

--- index.js
+++ index.js
@@ -15,12 +15,16 @@ app.use(session({
     secret: 'keyboard cat',
     cookie: { maxAge: 60000 }
 }))
+
+// handle CORS
+app.use(cors())
 // middelware
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('views', __dirname = './src/views')
 app.set('view engine', 'twig')
+
+
 
 
 app.get('/', (req, res, next) => {
@@ -36,3 +40,4 @@ app.use('/auth', authCtrl)
 
 
 app.listen(4000);
+console.log("👂 on port 4000");
